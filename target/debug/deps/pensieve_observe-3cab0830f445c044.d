f3671bd247350841c3a528203efeddbe3601fbd9
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2025-12-15 12:55:56 +0100

    gitignore and remove target

--- target/debug/deps/pensieve_observe-3cab0830f445c044.d
+++
@@ -1,9 +0,0 @@
-/Users/jeff/code/pensieve/target/debug/deps/pensieve_observe-3cab0830f445c044.d: crates/pensieve-observe/src/lib.rs Cargo.toml
-
-/Users/jeff/code/pensieve/target/debug/deps/libpensieve_observe-3cab0830f445c044.rmeta: crates/pensieve-observe/src/lib.rs Cargo.toml
-
-crates/pensieve-observe/src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
